478b027dd856b0a032fe57b2532292b98a5240ff
Vivek-4321 <vivekvenugopal514@gmail.com>
2025-03-04 10:45:04 +0530

    some changes

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
 import client from "./src/db/conn.js";
-import { Application } from 'oak';
-import { oakCors } from 'oakCors';
-import {authRouter} from "./src/main/routes.js";
+import { Application } from "oak";
+import { oakCors } from "oakCors";
+import { authRouter } from "./src/main/routes.js";
 import { config } from "dotenv";
-import {fileRouter} from "./src/main/routes.js";
-import {medicalInfoRouter} from "./src/main/routes.js"; 
-config({path:"./.env"});
+import { fileRouter } from "./src/main/routes.js";
+import { medicalInfoRouter } from "./src/main/routes.js";
+config({ path: "./.env" });
 const app = new Application();
 // Enable CORS for all routes
 // Configure CORS with specific options
-app.use(oakCors({
-    origin: "https://aethercure.vercel.app", // Your frontend URL
-    credentials: true, // Allow credentials
+app.use(
+  oakCors({
+    origin: (requestOrigin) => {
+      const allowedOrigins = [
+        "https://aethercure.vercel.app",
+        "http://localhost:5173",
+      ];
+      return allowedOrigins.includes(requestOrigin) ? requestOrigin : false;
+    },
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-  }));
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(authRouter.routes());
 app.use(authRouter.allowedMethods());
@@ -27,5 +35,5 @@ app.use(fileRouter.allowedMethods());
 app.use(medicalInfoRouter.routes());
 app.use(medicalInfoRouter.allowedMethods());
 
-console.log(`Server is running on http://localhost:${Deno.env.get('PORT')}/`);
-await app.listen({ port: parseInt(Deno.env.get('PORT')) });
+console.log(`Server is running on http://localhost:${Deno.env.get("PORT")}/`);
+await app.listen({ port: parseInt(Deno.env.get("PORT")) });
